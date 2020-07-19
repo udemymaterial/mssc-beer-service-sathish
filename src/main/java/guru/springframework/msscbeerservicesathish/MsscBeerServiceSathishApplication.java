acb2f4fe678d77484b76b587447ec86197259cba
sathishkumarpkd <sathishkumarpkd@gmail.com>
2020-07-19 12:06:49 +0530

    initial set up

---
+++ src/main/java/guru/springframework/msscbeerservicesathish/MsscBeerServiceSathishApplication.java
@@ -0,0 +1,13 @@
+package guru.springframework.msscbeerservicesathish;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MsscBeerServiceSathishApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MsscBeerServiceSathishApplication.class, args);
+    }
+
+}
